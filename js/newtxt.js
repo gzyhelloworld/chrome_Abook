324f4a0972e9052d76728c941c2ccec9b143eadf
gzyhelloworld <1226056642@qq.com>
2017-04-11 10:19:29 +0800

    change

--- js/newtxt.js
+++ js/newtxt.js
@@ -1,43 +1,56 @@
-if(typeof localStorage.txt == 'undefined') {localStorage.txt = '[]';localStorage.time = (new Date()).getDate();}
-if(typeof localStorage.strongtxt == 'undefined') localStorage.strongtxt = '[]';
-if(localStorage.time != (new Date()).getDate()){localStorage.txt = '[]';localStorage.time = (new Date()).getDate();}
+if(typeof localStorage.txt == 'undefined') {localStorage.txt = '[]';}
+var time = (new Date()).getDate();
+if(typeof localStorage.time == 'undefined' || localStorage.time != time){
+	localStorage.time = time;
+	var arr = JSON.parse(localStorage.txt);
+	for(var a=0;a<arr.length;a++){
+		if(arr[a].day!=9) arr[a].day--;
+	if(arr[a].day == 0){
+		arr.splice(a,1);
+	}
+	}
+	localStorage.txt = JSON.stringify(arr);
+}
 chrome.tabs.getSelected(null, function(tab){
 document.getElementsByTagName('input')[0].value = tab.url; 
 localStorage.url =  tab.url;
 localStorage.title = tab.title;});
-
-Vue.component('strongtxt',{
-	template:'<p><a :href="item.url" target="_blank">{{item.title}}</a><i v-on:click="delthis(index)"></i></p>',
-	props:['item','index'],
-	methods:{
-		delthis:function (index) {
-			var arr = JSON.parse(localStorage.strongtxt);
-			arr.splice(index,1);
-			localStorage.strongtxt = JSON.stringify(arr);
-			vm.strongtxts = arr;			
-		}
-	}
-});
-Vue.component('smalltxt',{
-	template:'<p><a :href="item.url" target="_blank">{{item.title}}</a><i v-on:click="delthis(index)"></i></p>',
+var smalltxt =  Vue.extend({
+	template:'<p><a :href="item.url" target="_blank">{{item.title}}</a><i v-on:click="delthis(index)"></i><strong v-on:click="addday(index)">{{item.day}}</strong><i @click="upthis(index)"></i></p>',
 	props:['item','index'],
 	methods:{
 		delthis:function (index) {
 			var arr = JSON.parse(localStorage.txt);
 			arr.splice(index,1);
-			localStorage.txt = JSON.stringify(arr);
 			vm.txt = arr;			
+		},
+		addday:function (index) {
+			var arr = JSON.parse(localStorage.txt);
+			var num = arr[index].day;			
+			num++;
+			arr[index].day =  (num>9?9:num);
+			vm.txt = arr;						
+		},
+		upthis:function (index) {
+			var arr = JSON.parse(localStorage.txt);
+			if(index != 0 ){
+				arr[index] = arr.splice(index-1, 1, arr[index])[0];
+			}
+			vm.txt = arr;
 		}
-	}
+	},
 });
 var vm = new Vue({
 	el:'#body',
+    components: {
+      smalltxt: smalltxt,
+    },	
 	data:{
+		msg:'blue',
 		check : false,
 		redcolor : false,
-		msg:'临时',
+		daycolor:["#aaa","#0f0","#00f","#f00","#ff0","#0ff","#f0f","#fcd","#cdf"],
 		txt:JSON.parse(localStorage.txt),
-		strongtxts:JSON.parse(localStorage.strongtxt),
 	},
 	methods:{
 		addurl:function () {
@@ -74,29 +87,23 @@ var vm = new Vue({
 		    }
 		    xhr.send();			
 		},
-		addtxt:function () {
-			var local = (vm.msg=='永久'?localStorage.strongtxt:localStorage.txt);
-			if(local.indexOf(localStorage.url)<=0){
-				var news = {url:localStorage.url,title:localStorage.title};
-				var newarr = JSON.parse(local);
+		addtxt:function () {			
+			if(localStorage.txt.indexOf(localStorage.url)<=0){
+				var news = {url:localStorage.url,title:localStorage.title,day:1};
+				var newarr = JSON.parse(localStorage.txt);
 				newarr.push(news);
-				if(vm.msg=='永久'){
-					localStorage.strongtxt = JSON.stringify(newarr);
-					vm.strongtxts = newarr;
-				}else{
-					localStorage.txt = JSON.stringify(newarr);
-					vm.txt = newarr;
-				}
+				vm.txt = newarr;			
 			}
-			this.choose = false;
-		},
-		change:function () {
-			vm.msg = (vm.msg=="临时"?"永久":"临时");	
-		},
+		}
 	},
 	computed:{
 		nulltrue:function () {
-			return this.strongtxts.length == 0 && this.txt.length == 0;
+			return this.txt.length == 0;
+		}
+	},
+	watch:{
+		txt:function (val) {
+			localStorage.txt = JSON.stringify(val);
 		}
 	}
 });
